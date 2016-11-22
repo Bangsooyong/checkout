0f11c0cb720e26d137c2046036832854c68af1e3
pkmzx <pkmzx@10.211.55.8>
2016-11-22 14:06:30 +0900

    login_register에 이메일 인증 통합..

--- ShoppingMall_ver1/src/main/java/com/shop/app/controller/BuyerController.java
+++ ShoppingMall_ver1/src/main/java/com/shop/app/controller/BuyerController.java
@@ -1,12 +1,15 @@
 package com.shop.app.controller;
 
 import java.io.IOException;
+import java.net.URLDecoder;
 
 import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,7 +26,12 @@ public class BuyerController {
 	private static final Logger logger = LoggerFactory.getLogger(BuyerController.class);
 	
 	@Autowired
+	private JavaMailSenderImpl mailSender;
+	
+	@Autowired
 	BuyerService buyerService;
+	
+	
 	
 	// 회원가입 양식 호출
 	@RequestMapping(value="/register", method=RequestMethod.GET)
@@ -31,8 +39,8 @@ public class BuyerController {
 		return "login_register";
 	}
 
+//////////////////////////////////////////////////////////////
 	// login_register 아이디 중복체크 컨트롤러
-
 	@RequestMapping(value="/checkid", method=RequestMethod.POST)
 	public void checkid(@RequestBody String userid, HttpServletResponse response) throws IOException{
 		
@@ -44,6 +52,7 @@ public class BuyerController {
 		
 		// DB에서 입력한 문자열 검색
 		BuyerVO vo = buyerService.read(b_id);
+		
 		// DB에 있다면 중복...
 		if (vo!=null){
 			String selectedID = vo.getB_id();
@@ -52,6 +61,7 @@ public class BuyerController {
 		}
 	}
 	
+	////////////////////////////////////////////////////////////////
 	// 회원가입 컨트롤러
 	@RequestMapping(value="/login_result", method=RequestMethod.POST)
 	public String login_result(BuyerVO vo){
@@ -61,12 +71,14 @@ public class BuyerController {
 		logger.info("회원가입 성공! ");
 		return "login_result";
 	}
+	
+/*	
 	@RequestMapping(value="login", method=RequestMethod.GET)
 	public void openLoginJSP(){
 		
 	}
-	
-	@RequestMapping(value="login", method=RequestMethod.POST)
+	*/
+/*	@RequestMapping(value="login", method=RequestMethod.POST)
 	public void login(String b_id, String b_pw){	
 		logger.info("login 컨트롤러 실행");
 		logger.info("b_id : "+b_id+" , b_pw : "+b_pw);
@@ -75,6 +87,67 @@ public class BuyerController {
 		} else {
 			logger.info("로그인 실패");
 		}
+	}*/
+	
+////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	// 인증번호 발송
+	@RequestMapping(value = "/checkemail", method = RequestMethod.POST)
+	public void checkEmail(@RequestBody String email, HttpServletResponse response) throws IOException {
+		logger.info("email: " + email);
+		
+		// @ converted to %40 in HTTPPost request
+		String convert_email = URLDecoder.decode(email, "UTF-8"); 
+		
+		// 필요없는 문자열을 제거
+		String b_email = convert_email.substring(0, convert_email.length()-1);
+		
+		// 4자리 인증번호 생성
+		// 1. 0~9999 까지의 난수를 발생시킨 후 1~3자리 수를 없애기위해 1000을 더해준다 (1000~10999)
+		// 2. 다섯자리가 넘어가면 1000을 빼준다.			
+		int code = (int) (Math.random() * 10000 + 1000); 
+		if (code > 10000){
+			code = code - 1000;
+		}
+		
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(b_email); // 받는 이메일 등록
+		
+			logger.info("메일 주소 : " + b_email);
+		
+		message.setSubject("쇼핑몰 인증번호");  // 이메일 제목
+		message.setText("인증번호 : " + code);  // 이메일 내용
+		
+			logger.info("보낸 코드 : " + code);
+		
+		mailSender.send(message);  // 이메일 전송
+		// model.addAttribute("code", code);
+		
+		response.getWriter().print(code);
+		//return "email_result";
 	}
 	
-}
+/*	@RequestMapping(value="/checkcode", method=RequestMethod.POST)
+	public void checkCode(@RequestBody String code, HttpServletResponse response) throws IOException{
+		
+		logger.info("checkCode 실행");
+
+		
+		// 필요없는 문자열을 제거
+		String b_id = userid.substring(0, userid.length()-1);
+		
+		// DB에서 입력한 문자열 검색
+		BuyerVO vo = buyerService.read(b_id);
+		
+		// DB에 있다면 중복...
+		if (vo!=null){
+			String selectedID = vo.getB_id();
+			logger.info("selectedID"+selectedID);
+			response.getWriter().print(1);
+		}
+	}*/
+	
+	
+	
+	
+} // end class
