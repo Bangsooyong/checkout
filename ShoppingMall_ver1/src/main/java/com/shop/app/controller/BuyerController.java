c60506b7a2ea05b4038f44f5b107c557834a4a82
taehun1214 <taehun1214@naver.com>
2016-11-22 14:07:22 +0900

    인터셉터 복붙

--- ShoppingMall_ver1/src/main/java/com/shop/app/controller/BuyerController.java
+++ ShoppingMall_ver1/src/main/java/com/shop/app/controller/BuyerController.java
@@ -2,7 +2,9 @@ package com.shop.app.controller;
 
 import java.io.IOException;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,7 +34,6 @@ public class BuyerController {
 	}
 
 	// login_register 아이디 중복체크 컨트롤러
-
 	@RequestMapping(value="/checkid", method=RequestMethod.POST)
 	public void checkid(@RequestBody String userid, HttpServletResponse response) throws IOException{
 		
@@ -67,11 +68,13 @@ public class BuyerController {
 	}
 	
 	@RequestMapping(value="login", method=RequestMethod.POST)
-	public void login(String b_id, String b_pw){	
+	public void login(String b_id, String b_pw, HttpServletRequest request, String query){	
 		logger.info("login 컨트롤러 실행");
 		logger.info("b_id : "+b_id+" , b_pw : "+b_pw);
 		if (buyerService.isValidUser(b_id, b_pw)){
 			logger.info("로그인 성공");
+			HttpSession session = request.getSession(true);
+			session.setAttribute("loginInfo", query);
 		} else {
 			logger.info("로그인 실패");
 		}
